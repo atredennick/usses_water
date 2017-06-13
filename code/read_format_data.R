88f15fbb0971272bfb6c744943ed1e2d665e0695
Andrew Tredennick <atredenn@gmail.com>
2017-06-13 17:10:10 -0600

    bringing in soil moisture data for issue #3; still need to discuss aggregation

--- code/read_format_data.R
+++ code/read_format_data.R
@@ -25,12 +25,27 @@ permanent_quad_biomass <- readRDS(paste0(data_path,anpp_fname))
 weather <- read.csv("../data/weather/ClimateIPM.csv") %>%
   select(-pptLag,-ppt2,-TmeanSpr1,-TmeanSpr2)
 
+soil_moisture <- read.csv("../data/soil_moisture_data/average_seasonal_soil_moisture.csv") %>%
+  select(-year) %>%
+  separate(simple_date, c("year","month","day")) %>%
+  group_by(year,month,Treatment,type,year) %>%
+  summarise(avg_vwc = mean(VWC,na.rm=TRUE)) %>%
+  filter(type=="predicted") %>%
+  mutate(month_year = as.factor(paste0(year,"-",month))) %>%
+  filter(month %in% c("04","05","06")) %>%
+  group_by(year,Treatment) %>%
+  summarise(total_seasonal_vwc = sum(avg_vwc)) %>%
+  ungroup() %>%
+  mutate(year = as.numeric(year))
+
+
 anpp_data <- permanent_quad_biomass %>% 
   filter(Treatment %in% c("Control","Drought","Irrigation")) %>%
   filter(!str_detect(quadname, 'P1|P7')) %>%
   filter(year > 2011) %>%
   rename(anpp = biomass_grams_est) %>%
   left_join(weather, by = "year") %>%
+  left_join(soil_moisture, by = c("year","Treatment")) %>%
   select(-QuadName,-quad,-Grazing,-paddock,-ndvi) %>%
   mutate(ppt1_scaled = as.numeric(scale(ppt1)),
          year_id = year - 2011)
@@ -42,4 +57,11 @@ drought_data <- anpp_data %>%
 irrigate_data <- anpp_data %>%
   filter(Treatment != "Drought") %>%
   mutate(trt_id = as.numeric(as.factor(Treatment)) - 1)
+
+# ggplot(anpp_data, aes(total_seasonal_vwc, log(anpp), color=Treatment))+
+#   geom_point()+
+#   stat_smooth(se=F,method="lm")+
+#   stat_smooth(se=F,color="black",method="lm")
+
+
 
