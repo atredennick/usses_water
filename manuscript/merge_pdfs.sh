18d0ffdc1837ecf13d46718c854ad44ee47fe136
Andrew Tredennick <atredenn@gmail.com>
2017-08-25 17:43:58 -0600

    final edits for coauthors

---
+++ manuscript/merge_pdfs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o usses_water_ms_appendices.pdf usses_water_ms.pdf Appendix1.pdf Appendix2.pdf Appendix3.pdf
