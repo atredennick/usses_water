c465075deb4d5b3eb9bf6c50d1f2f2d402ca8134
Andrew Tredennick <atredenn@gmail.com>
2018-01-19 11:56:16 -0700

    finalish version sent to PBA

--- manuscript/merge_pdfs.sh
+++ manuscript/merge_pdfs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o usses_water_ms_appendices.pdf usses_water_ms.pdf Appendix1.pdf Appendix2.pdf Appendix3.pdf
+"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o usses_water_ms_appendices.pdf usses_water_ms.pdf Appendix1.pdf Appendix2.pdf Appendix3.pdf Appendix4.pdf Appendix5.pdf Appendix6.pdf
